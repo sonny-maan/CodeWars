c3a0a3c9fbefc5d12f56ff6380fe7959f5a62feb
Amritpal Maan <sonny_maan@yahoo.com>
2019-09-26 21:26:45 +0100

    refactored code

--- who_likes_it/lib/app.rb
+++ who_likes_it/lib/app.rb
@@ -1,9 +1,15 @@
 # frozen_string_literal: true
-
 def likes(names)
-  return 'no one likes this' if names.empty? == true
-  return "#{names[0]} likes this" if names.count == 1
-  return "#{names[0]} and #{names[1]} like this" if names.count == 2
-  return "#{names[0]}, #{names[1]} and #{names[2]} like this" if names.count == 3
-  return "#{names[0]}, #{names[1]} and #{names.count - 2} others like this" if names.count > 3
+  case names.size
+  when 0
+    "no one likes this"
+  when 1
+    "#{names[0]} likes this"
+  when 2
+    "#{names[0]} and #{names[1]} like this"
+  when 3
+    "#{names[0]}, #{names[1]} and #{names[2]} like this"
+  else
+    "#{names[0]}, #{names[1]} and #{names.size - 2} others like this"
+  end
 end
