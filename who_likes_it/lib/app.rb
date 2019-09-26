53e610fd05c47c7e46fc291c9e324630e5c9b20f
Amritpal Maan <sonny_maan@yahoo.com>
2019-09-26 21:18:41 +0100

    ran linter

--- who_likes_it/lib/app.rb
+++ who_likes_it/lib/app.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 def likes(names)
-  return "no one likes this" if names.empty? == true
+  return 'no one likes this' if names.empty? == true
   return "#{names[0]} likes this" if names.count == 1
-  return "#{names[0]} and #{names[1]} like this" if names.count ==2
-  return "#{names[0]}, #{names[1]} and #{names[2]} like this" if names.count ==3
+  return "#{names[0]} and #{names[1]} like this" if names.count == 2
+  return "#{names[0]}, #{names[1]} and #{names[2]} like this" if names.count == 3
   return "#{names[0]}, #{names[1]} and #{names.count - 2} others like this" if names.count > 3
 end
--- who_likes_it/spec/app_spec.rb
+++ who_likes_it/spec/app_spec.rb
@@ -1,26 +1,26 @@
+# frozen_string_literal: true
+
 require './lib/app.rb'
 
 describe 'likes(names)' do
-
-it 'has a message for no likes' do
- names = []
-  expect(likes(names)).to eq "no one likes this"
-end
-it 'has a message for 1 like' do
- names = ['Sonny']
-  expect(likes(names)).to eq "Sonny likes this"
-end
-it 'has a message for 2 likes' do
- names = ['Sonny', 'Joe Rogan']
-  expect(likes(names)).to eq "Sonny and Joe Rogan like this"
-end
-it 'has a message for 3 likes' do
- names = ['Sonny', 'Joe Rogan', 'Seth Rogen']
-  expect(likes(names)).to eq "Sonny, Joe Rogan and Seth Rogen like this"
-end
-it 'has a message over 3 likes' do
- names = ['Sonny', 'Joe Rogan', 'Seth Rogen', 'Big Man Like Shaq']
-  expect(likes(names)).to eq "Sonny, Joe Rogan and 2 others like this"
-end
-
+  it 'has a message for no likes' do
+    names = []
+    expect(likes(names)).to eq 'no one likes this'
+  end
+  it 'has a message for 1 like' do
+    names = ['Sonny']
+    expect(likes(names)).to eq 'Sonny likes this'
+  end
+  it 'has a message for 2 likes' do
+    names = ['Sonny', 'Joe Rogan']
+    expect(likes(names)).to eq 'Sonny and Joe Rogan like this'
+  end
+  it 'has a message for 3 likes' do
+    names = ['Sonny', 'Joe Rogan', 'Seth Rogen']
+    expect(likes(names)).to eq 'Sonny, Joe Rogan and Seth Rogen like this'
+  end
+  it 'has a message over 3 likes' do
+    names = ['Sonny', 'Joe Rogan', 'Seth Rogen', 'Big Man Like Shaq']
+    expect(likes(names)).to eq 'Sonny, Joe Rogan and 2 others like this'
+  end
 end
