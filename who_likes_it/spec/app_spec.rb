eaa1b44b7afcd2305f9cdc73268d4494dfcc062d
Amritpal Maan <sonny_maan@yahoo.com>
2019-09-26 21:07:55 +0100

    wrote test for 2 likes

--- who_likes_it/spec/app_spec.rb
+++ who_likes_it/spec/app_spec.rb
@@ -6,7 +6,14 @@ it 'has a message for no likes' do
  names = []
   expect(likes(names)).to eq "no one likes this"
 end
-
+it 'has a message for 1 like' do
+ names = ['Peter']
+  expect(likes(names)).to eq "Peter likes this"
+end
+it 'has a message for 2 likes' do
+ names = ['Peter', 'James']
+  expect(likes(names)).to eq "Peter and James like this"
+end
 
 
 end
