fe0297fbcf143b6023365398facebf88ba0d5fb3
Amritpal Maan <sonny_maan@yahoo.com>
2019-09-28 00:40:31 +0100

    wrote tests

--- build_tower/spec/app_spec.rb
+++ build_tower/spec/app_spec.rb
@@ -2,7 +2,14 @@
 
 require './lib/app.rb'
 
-describe '' do
-  it '' do
+describe 'towerBuilder' do
+  it 'prints 1 floor' do
+    expect(towerBuilder(1)).to eq ['*']
+  end
+  it 'prints 2 floors' do
+    expect(towerBuilder(2)).to eq [' * ', '***']
+  end
+  it 'prints 3 floors' do
+    expect(towerBuilder(3)).to eq ['  *  ', ' *** ', '*****']
   end
 end
